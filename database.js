f363f1d33ab749add62ac623d03fac8305109c83
mohaamdtiti-source <mohaamdtiti@gmail.com>
2026-06-11 00:02:58 +0330

    Update database.js

--- database.js
+++ database.js
@@ -1,24 +1,30 @@
 const sqlite3 = require("sqlite3").verbose();
 
-const db = new sqlite3.Database("./missilewar.db");
+const db = new sqlite3.Database("missilewar.db");
 
-db.serialize(function () {
+db.serialize(() => {
 
 db.run(
-    "CREATE TABLE IF NOT EXISTS users (" +
-    "id INTEGER PRIMARY KEY AUTOINCREMENT," +
-    "username TEXT UNIQUE," +
-    "password TEXT," +
-    "money INTEGER DEFAULT 1000," +
-    "iron INTEGER DEFAULT 500," +
-    "fuel INTEGER DEFAULT 300," +
-    "missiles INTEGER DEFAULT 0," +
-    "level INTEGER DEFAULT 1," +
-    "factory INTEGER DEFAULT 1," +
-    "fuelPlant INTEGER DEFAULT 1," +
-    "defense INTEGER DEFAULT 1" +
-    ")"
+CREATE TABLE IF NOT EXISTS users (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+username TEXT UNIQUE,
+password TEXT,
+
+money INTEGER DEFAULT 1000,
+iron INTEGER DEFAULT 500,
+fuel INTEGER DEFAULT 300,
+
+missiles INTEGER DEFAULT 0,
+
+level INTEGER DEFAULT 1,
+
+factory INTEGER DEFAULT 1,
+fuelPlant INTEGER DEFAULT 1,
+defense INTEGER DEFAULT 1
+)
 );
+
+console.log("Database loaded");
 
 });
 
